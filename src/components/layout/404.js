8d8cbb2f8d4f1fbc3682141683f9d23af5766b69
Jaime Duque <jaime.duque@estratek.com.co>
2020-04-27 01:49:14 -0500

    The component is modified

--- src/components/layout/404.js
+++ src/components/layout/404.js
@@ -4,11 +4,16 @@ import styled from 'styled-components';
 const H1 = styled.h1`
     margin-top: 5rem;
     text-align: center;
+    font-size: 2rem;
 `
 
 const Eror404 = () => {
     return (
-        <H1>Page not exist</H1>
+        <H1>
+            <strong>
+                <span role="img" aria-label="sad-face"> 😞 </span>Sorry, results not found!
+            </strong>
+        </H1>
     );
 }
 
